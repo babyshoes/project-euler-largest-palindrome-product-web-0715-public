bb54f411e3340112e2a0c5561a3d1026974f3580
babyshoes <dianecai@gmail.com>
2015-10-13 16:23:16 -0400

    racecar

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+require 'pry'
+def is_palindrome?(input)
+  input.to_s == input.to_s.reverse
+end
+
+def largest_palindrome_product
+  find_palindrome_products
+end
+
+def find_palindrome_products
+  num1 = num2 = 999
+  arr = []
+  while num1 > 100
+    while num2 > 100
+      product = num1 * num2
+      arr << product if is_palindrome?(product)
+      num2 -= 1
+    end
+    num2 = 999
+    num1 -= 1
+  end
+  arr.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,34 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  attr_accessor :num1, :num2, :arr
+  def initialize
+    @num1 = 999
+    @num2 = 999
+    @arr = []
+  end
+
+  def is_palindrome?(input)
+    input.to_s == input.to_s.reverse
+  end
+
+  def find_palindrome_products
+    while num1 > 100
+      while num2 > 100
+        yield
+      end
+      self.num2 = 999
+      self.num1 -= 1
+    end
+    arr.max
+  end
+
+  def shovel_product_if_palindrome
+    product = num1 * num2
+    arr << product if is_palindrome?(product)
+    self.num2 -= 1
+  end
+
+  def answer
+    find_palindrome_products { shovel_product_if_palindrome }
+  end
+end
